8b90c14cf0795852d34b0985d47c3c45dbee608b
Pedro-En <characayo.pedro@ficct.uagrm.edu.bo>
2023-06-22 00:37:29 -0400

    config 4

--- src/libs/sequelize.js
+++ src/libs/sequelize.js
@@ -1,18 +1,18 @@
 
 const { Sequelize } = require('sequelize');
-
+require('dotenv').config();
 const { config } = require('../config/config');
 const setupModels = require('./../db/models/index');
 
-const sequelize = new Sequelize(
-    config.dbName,
-    config.dbUser,
-    config.dbPassword,
-    {
-        host:config.dbHost,
-        dialect: 'mysql',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false,        
+        }
     }
-);
+ });
 
 sequelize.sync();
 setupModels(sequelize);
